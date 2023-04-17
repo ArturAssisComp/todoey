b800125ce63236b9c23eb3fa937f21f743e0f43f
Artur Assis Alves <arturassiscomp@gmail.com>
2023-04-17 12:27:19 -0300

    init project

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<Data>(
+      builder: (context) => Data(),
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: MyText(),
+          ),
+          body: Level1(),
+        ),
+      ),
+    );
+  }
+}
+
+class Level1 extends StatelessWidget {
+  const Level1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Level2();
+  }
+}
+
+class Level2 extends StatelessWidget {
+  const Level2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        MyTextField(),
+        Level3(),
+      ],
+    );
+  }
+}
+
+class Level3 extends StatelessWidget {
+  const Level3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(Provider.of<Data>(context).data);
+  }
+}
+
+class MyText extends StatelessWidget {
+  const MyText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(Provider.of<Data>(context, listen: false).data);
+  }
+}
+
+class MyTextField extends StatelessWidget {
+  const MyTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: (newText) {
+        Provider.of<Data>(context).changeString(newText);
+      },
+    );
+  }
+}
+
+class Data extends ChangeNotifier {
+  String data = 'Some data';
+
+  void changeString(String newString) {
+    data = newString;
+    notifyListeners();
+  }
+}
