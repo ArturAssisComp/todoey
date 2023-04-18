830ca545b3b8e8500bbe91655549f6289b39ade0
Artur Assis Alves <arturassiscomp@gmail.com>
2023-04-18 11:31:09 -0300

    Implement task checking feature.

--- lib/user_interface/widgets/task.dart
+++ lib/user_interface/widgets/task.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Task extends StatelessWidget {
+class Task extends StatefulWidget {
   const Task({
     super.key,
     required this.description,
@@ -8,13 +8,53 @@ class Task extends StatelessWidget {
   final String description;
 
   @override
+  State<Task> createState() => _TaskState();
+}
+
+class _TaskState extends State<Task> {
+  bool _isChecked = false;
+  @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text(description),
-      trailing: Checkbox(
-        value: false,
-        onChanged: (newValue) {},
+      onTap: () {
+        setState(() {
+          _isChecked = !_isChecked;
+        });
+      },
+      title: Text(
+        widget.description,
+        style: TextStyle(
+          decoration: _isChecked ? TextDecoration.lineThrough : null,
+        ),
       ),
+      trailing: TaskCheckbox(
+        value: _isChecked,
+        onChanged: (newValue) {
+          if (newValue != null) {
+            setState(() {
+              _isChecked = newValue;
+            });
+          }
+        },
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  const TaskCheckbox({
+    super.key,
+    required this.value,
+    this.onChanged,
+  });
+  final bool value;
+  final void Function(bool?)? onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: value,
+      onChanged: onChanged,
     );
   }
 }
